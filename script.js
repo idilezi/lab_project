01ce050dbccdde15838f203319f1871f11590d11
idilezi <malina271106@gmail.com>
2026-04-13 20:26:09 +0300

    lab 13

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ const swiper = new Swiper(".mySwiper", {
         1024: { slidesPerView: 2 } 
     } 
 });
+
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+    
+    if (mobileMenu.classList.contains("active")) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
+});
+
+const menuLinks = document.querySelectorAll(".mobile-menu a");
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        mobileMenu.classList.remove("active");
+        document.body.style.overflow = 'auto';
+    });
+});
