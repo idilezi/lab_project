b52193914ca2e8af97f79c3d645d00d5428557ce
idilezi <malina271106@gmail.com>
2026-04-14 08:55:19 +0300

    lab 15

--- script.js
+++ script.js
@@ -40,3 +40,35 @@ menuLinks.forEach(link => {
         document.body.style.overflow = 'auto';
     });
 });
+
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        tabBtns.forEach(item => item.classList.remove('active'));
+        tabContents.forEach(item => item.classList.remove('active'));
+
+        btn.classList.add('active');
+        const tabId = btn.getAttribute('data-tab');
+        document.getElementById(tabId).classList.add('active');
+    });
+});
+
+const modal = document.getElementById('my-modal');
+const openBtn = document.getElementById('open-modal-btn');
+const closeBtn = document.querySelector('.close-modal');
+
+openBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('open');
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.remove('open');
+    }
+});
